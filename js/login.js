cdb51de44eeca8e71d9f01507131afad1c2f3bb7
rominaferreira <romina.gfp@gmail.com>
2022-08-21 19:13:02 -0300

    Entrega 1
    Modificaciones realizadas para la primer entrega.

---
+++ js/login.js
@@ -0,0 +1,19 @@
+function handleSubmit() {
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+
+    if (
+        !email ||
+        !password
+
+    ) {
+        alert("Ingresa tus datos");
+
+    } else {
+        window.location.href = "https://rominaferreira.github.io/E-commerce_JAP/";
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("regBtn").addEventListener("click", handleSubmit);
+});
---
+++ js/products.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    fetch('https://japceibal.github.io/emercado-api/cats_products/101.json')
+
+        .then(respuesta => respuesta.json()) // Recibe objeto de tipo response y devuelve objeto transformado en Javascript
+
+        .then(data => {
+
+            let htmlContentToAppend = `<h1 class="text-center">Productos</h1><p class="text-center"> Verás aquí todos los productos de la categoría Autos</p>`;
+
+
+            for (let i = 0; i < data.products.length; i++) {
+                let product = data.products[i];
+
+                htmlContentToAppend +=
+
+                `<div id="${product.id}" class="row mb-1">
+                <img src=" ${product.image}" class="image-size">
+                <div class="col">
+                 <div class="row">${product.name} - ${product.currency} ${product.cost}</div>
+                 <div class="row">${product.description}</div>
+                 </div>
+                                  
+                 Vendidos ${product.soldCount}
+                                      
+                 
+                </div> `
+
+            }
+
+            document.getElementById("product-list").innerHTML = htmlContentToAppend;
+        });
+});
