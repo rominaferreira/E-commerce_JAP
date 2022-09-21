bbf3cb8abea684557ea0ce065cfc6ea046031d09
rominaferreira <romina.gfp@gmail.com>
2022-09-20 22:46:27 -0300

    Avances entrega 3_ Comentarios compradores

--- js/product-info.js
+++ js/product-info.js
@@ -1,17 +1,19 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function () {
+
+    // Información del producto
 
     let htmlContentToAppend = ""
-    
-    fetch(`https://japceibal.github.io/emercado-api/products/${window.localStorage.getItem("productID")}.json`) 
 
-    .then(respuesta => respuesta.json()) 
+    fetch(`https://japceibal.github.io/emercado-api/products/${window.localStorage.getItem("productID")}.json`)
 
-    .then(data => {
+        .then(respuesta => respuesta.json())
 
-        let product=data
-        htmlContentToAppend +=
-        
-        `<h2>${product.name}</h2>
+        .then(data => {
+
+            let product = data
+            htmlContentToAppend +=
+
+                `<h2>${product.name}</h2>
         <hr>
         <b> Precio </b>
         <div>${product.currency} ${product.cost} </div>
@@ -28,16 +30,57 @@ document.addEventListener("DOMContentLoaded", function(){
         <div> <b>Imágenes</b>
         <br>
         `
-        let imagenes = product.images;
-        for (let imagen of imagenes) { 
-        htmlContentToAppend += `<img src="${imagen}" class="image-size">`
-        }
-        `</div>`;
-              
-        document.getElementById("product-info").innerHTML = htmlContentToAppend;
-    })
+            let imagenes = product.images;
+            for (let imagen of imagenes) {
+                htmlContentToAppend += `<img src="${imagen}" class="image-size">`
+            }
+            `</div>`;
 
-    
-    
+            document.getElementById("product-info").innerHTML = htmlContentToAppend;
+        })
+
+    //Comentarios del producto
+
+    let htmlComentarios = ""
+
+    fetch(`https://japceibal.github.io/emercado-api/products_comments/${window.localStorage.getItem("productID")}.json`)
+
+        .then(respuesta => respuesta.json())
+
+        .then(data => {
+
+            let productsComments = data
+            htmlComentarios +=
+
+                ` <h3>Comentarios</h3>
+       <hr>`
+
+            productsComments.forEach(productComment => {
+
+                htmlComentarios +=
+
+                    `<b>${productComment.user}</b> ${productComment.dateTime}`
+
+                //Agrego estrellas de acuerdo al "score" 
+
+                for (let i = 1; i <= 5; i++) {
+
+                    if (i <= productComment.score) {
+                        htmlComentarios += `<span class="fa fa-star checked"></span>`
+                    }
+                    else {
+                        htmlComentarios += `<span class="fa fa-star"></span>`
+                    }
+                }
+
+                htmlComentarios += `${productComment.description}`
+
+            });
+
+
+            document.getElementById("product-comments").innerHTML = htmlComentarios;
+
+
+        })
+
 })
-       
