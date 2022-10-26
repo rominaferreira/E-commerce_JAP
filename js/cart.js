8973141a5038e0532a0cc408a7287e269b1eab93
rominaferreira <romina.gfp@gmail.com>
2022-10-26 20:55:03 -0300

    Nav bar cart

--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,6 @@
 //AGREGAR EL COSO EN EL NAVBAR: 
 //GET IN: Llama a lo anteriormente seteado en index, mediante la key "email"
-//document.getElementById("p-email").innerHTML = window.localStorage.getItem("email")
+document.getElementById("p-email").innerHTML = window.localStorage.getItem("email")
 
 //Funciones globales
 
