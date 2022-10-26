74f075aa1673b713da0f45010099fdad4c77e5ff
rominaferreira <romina.gfp@gmail.com>
2022-10-26 09:53:47 -0300

    Avances entrega 6

--- js/cart.js
+++ js/cart.js
@@ -6,18 +6,65 @@
 
 let costoProducto
 let currencyProducto
+let subtotal
+let costoEnvio
 
-//Llamado
+
 
 document.addEventListener("DOMContentLoaded",  function () { 
 
+//Escucha modificación del subtotal
     document.getElementById("cantidad").addEventListener("input", function(){
     
         let nuevaCantidad = document.getElementById ("cantidad").value
-        document.getElementById("subtotal").innerHTML =`${currencyProducto} ${costoProducto*nuevaCantidad}`
+        subtotal = costoProducto*nuevaCantidad
+        document.getElementById("subtotal").innerHTML =`${currencyProducto} ${subtotal}`
+
+        //Entregable 6
         
+        document.getElementById("productCost").innerHTML =`${currencyProducto} ${subtotal}`
+        
+        if (document.getElementById("flexRadioDefault1").checked) {
+            costoEnvio= subtotal*0.15
+        } 
+        else if (document.getElementById("flexRadioDefault2").checked) {
+            costoEnvio= subtotal*0.07
+        } 
+        else if (document.getElementById("flexRadioDefault3").checked) {
+            costoEnvio= subtotal*0.05
+        }
+        
+        document.getElementById("deliverCost").innerHTML =`${currencyProducto} ${Math.round(costoEnvio)}`
+              
 
     });
+
+//Escucha modificación del tipo de envío seleccionado
+
+    document.getElementById("deliverType").addEventListener("change", function(event){
+   
+    const id = event.target.id
+      
+
+    if (id === 'flexRadioDefault1') {
+       
+        costoEnvio = subtotal*0.15
+       
+    }
+    
+    else if (id === 'flexRadioDefault2'){
+        costoEnvio = subtotal*0.07
+    }
+
+    else {
+       costoEnvio = subtotal*0.05
+    }    
+    
+
+    document.getElementById("deliverCost").innerHTML =`${currencyProducto} ${Math.round(costoEnvio)}`
+
+
+});
 
     //Fetch
 
@@ -29,12 +76,17 @@ document.addEventListener("DOMContentLoaded",  function () {
         .then(data => {
 
             document.getElementById("nombre").innerHTML = data.articles[0].name;
+            document.getElementById("costo").innerHTML = `${data.articles[0].currency} ${data.articles[0].unitCost}`;
             document.getElementById("imagen").src = data.articles[0].image;
             document.getElementById("cantidad").value = data.articles[0].count;
             costoProducto = data.articles[0].unitCost;
             currencyProducto = data.articles[0].currency;
             let cantidadProductoInicial = data.articles[0].count;
             document.getElementById("subtotal").innerHTML =`${currencyProducto} ${costoProducto*cantidadProductoInicial}`
+            
+            //Entregable 6
+            document.getElementById("productCost").innerHTML =`${currencyProducto} ${costoProducto*cantidadProductoInicial}`
+            subtotal = costoProducto*cantidadProductoInicial
             
             
         })
