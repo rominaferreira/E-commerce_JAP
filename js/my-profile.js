82e47901962379307118f88ec97cd1987b4d7561
rominaferreira <romina.gfp@gmail.com>
2022-11-15 22:06:15 -0300

    Avances entrega 7
    Controles gráficos para ingreso de datos del usuario

---
+++ js/my-profile.js
@@ -0,0 +1,4 @@
+//GET IN: Llama a lo anteriormente seteado en index, mediante la key "email"
+document.getElementById("p-email").innerHTML = window.localStorage.getItem("email")
+
+document.getElementById("email") = window.localStorage.getItem("email")
